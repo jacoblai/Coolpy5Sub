9ea86b4512288487d03d7b15631876305ad701d2
JacobLai <jacle169@21cn.com>
2016-07-24 15:20:16 +0800

    修正跨域http请求

--- src/Coolpy/Cors/Cors.go
+++ src/Coolpy/Cors/Cors.go
@@ -7,14 +7,10 @@ const (
 	allow_origin string = "Access-Control-Allow-Origin"
 	allow_methods string = "Access-Control-Allow-Methods"
 	allow_headers string = "Access-Control-Allow-Headers"
-	allow_credentials string = "Access-Control-Allow-Credentials"
-	expose_headers string = "Access-Control-Expose-Headers"
-	credentials string = "true"
 	origin string = "Origin"
-	methods string = "POST, GET, OPTIONS, PUT, DELETE, HEAD, PATCH"
-
+	methods string = "GET,PUT,POST,DELETE"
 	// If you want to expose some other headers add it here
-	headers string = "Accept, Accept-Encoding, Authorization, Content-Length, Content-Type, X-CSRF-Token"
+	headers string = "U-ApiKey,Authorization,Content-Length,Content-Type"
 )
 
 // Handler will allow cross-origin HTTP requests
@@ -30,8 +26,6 @@ func CORS(next http.Handler) http.Handler {
 		// Set other headers
 		w.Header().Set(allow_headers, headers)
 		w.Header().Set(allow_methods, methods)
-		w.Header().Set(allow_credentials, credentials)
-		w.Header().Set(expose_headers, headers)
 
 		// If this was preflight options request let's write empty ok response and return
 		if r.Method == options {
