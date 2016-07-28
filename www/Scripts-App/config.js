e4ad7959bf5851c2d6d57306b91754daebd924de
JacobLai <jacle169@21cn.com>
2016-07-28 20:21:46 +0800

    前端路由系统

--- www/Scripts-App/config.js
+++ www/Scripts-App/config.js
@@ -1,43 +1,8 @@
 ﻿var routes = [
-        new routing.routes.NavigationRoute("pages/total", "mainpage.html", {
-            cacheView: true, vmFactory: function (callback) {
-            callback(new viewModels.IndexVM());
-            }, title: "数据模型"
-        }),
-        new routing.routes.NavigationRoute("pages/index", "firstpage.html", {
+        new routing.routes.NavigationRoute("pages/index", "mg_users.html", {
             cacheView: true, isDefault: true, vmFactory: function (callback) {
-                callback(new viewModels.firstpageVM());
-            }, title: "一门式自然人库"
-        }),
-        new routing.routes.NavigationRoute("pages/pl/{uid}/{?value}", "personlabel.html?uid={uid}", {
-            cacheView: false, vmFactory: function (callback) {
-                callback(new viewModels.plVM());
-            }, title: "自然人标签"
-        }),
-        new routing.routes.NavigationRoute("pages/pf/{uid}/{?value}", "profile.html?uid={uid}", {
-            cacheView: false, vmFactory: function (callback) {
-                callback(new viewModels.pfVM());
-            }, title: "自然人祥细资料"
-        }),
-        new routing.routes.NavigationRoute("pages/more", "morepage.html", {
-            cacheView: true, vmFactory: function (callback) {
-                callback(new viewModels.moreVM());
-            }, title: "自人库展示"
-        }),
-        new routing.routes.NavigationRoute("pages/detailes/{skin}/{?index}/{?ps}", "viewdetailes.html?skin={skin}", {
-            cacheView: false, vmFactory: function (callback) {
-                callback(new viewModels.detailesVM());
-            }, title: "数据浏览"
-        }),
-        new routing.routes.NavigationRoute("pages/logs/{?skin}/{?index}/{?ps}", "viewlogs.html", {
-            cacheView: false, vmFactory: function (callback) {
-                callback(new viewModels.logsVM());
-            }, title: "日志浏览"
-        }),
-        new routing.routes.NavigationRoute("pages/dm", "datamodelpage.html", {
-            cacheView: false, vmFactory: function (callback) {
-                callback(new viewModels.dmVM());
-            }, title: "数据模型应用"
+                callback(new viewModels.IndexVM());
+            }, title: "用户管理"
         })
 ];
 var router = new routing.Router("views-placeholder", // ID of element in which will be loaded views.
